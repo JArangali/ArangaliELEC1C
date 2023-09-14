c364779c4c6944c8ebfa0a96f809c7da20986160
Jas Arangali <jasperlindell.arangali.cics@ust.edu.ph>
2023-09-14 19:53:06 +0800

    Arangali LAB ACT

---
+++ ArangaliITELEC1C/Controllers/InstructorController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ArangaliITELEC1C.Models;
+
+namespace ArangaliITELEC1C.Controllers;
+
+    public class InstructorController : Controller
+    {
+        List<Instructor> InstructorList = new List<Instructor>()
+        {
+            new Instructor()
+
+            {   Id = 1,
+                InstructorFirstName = "Jasper Lindell",
+                InstructorLastName = "Arangali",
+                DateHired = DateTime.Now,
+                InstructorIsTenured = true,
+                Rank = Rank.Instructor
+            },
+            new Instructor()
+            {
+                Id = 2,
+                InstructorFirstName = "Gabriel",
+                InstructorLastName = "Montano",
+                DateHired = DateTime.Parse("02/09/2019"),
+                InstructorIsTenured = true,
+                Rank = Rank.AssociateProfessor
+            },
+            new Instructor()
+            {
+                Id = 3,
+                InstructorFirstName = "Marcus Andre",
+                InstructorLastName = "Caronongan",
+                DateHired = DateTime.Parse("09/20/2003"),
+                InstructorIsTenured = false,
+                Rank = Rank.Professor
+            }
+        };
+        public IActionResult Index()
+        {
+            return View(InstructorList);
+        }
+
+    public IActionResult ShowDetail(int id)
+    {
+        //Search for the student whose id matches the given id
+        Instructor? instructor = InstructorList.FirstOrDefault(st => st.Id == id);
+
+        if (instructor != null)//was an instructor found?
+            return View(instructor);
+
+        return NotFound();
+    }
+}
---
+++ ArangaliITELEC1C/Controllers/StudentController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ArangaliITELEC1C.Models;
+
+namespace ArangaliITELEC1C.Controllers;
+
+public class StudentController : Controller
+{
+
+    public IActionResult Index()
+    {
+        Student st = new Student();
+        st.StudentId = 1;
+        st.StudentName = "Jasper Lindell Arangali";
+        st.DateEnrolled = DateTime.Parse("8/30/2023");
+        st.Course = Course.BSIT;
+        st.Email = "jasperlindell.arangali.cics@ust.edu.ph";
+
+        ViewBag.student = st;
+
+        return View();
+    }
+
+    public IActionResult Error()
+    {
+        return View();
+    }
+}
+
